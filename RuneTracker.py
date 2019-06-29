7321a2b35fa66adc56af1cee5627396ca7f3b94d
Fayez <fayezchedid@live.ca>
2019-06-29 01:29:12 -0400

    optimised imports

--- RuneTracker.py
+++ RuneTracker.py
@@ -1,4 +1,7 @@
-import requests, pandas, math, os
+from os import system
+from math import floor, ceil
+from requests import get
+from pandas import read_csv
 
 def main():
     print("\n"+30 * "-"+"MENU"+ 30 * "-")
@@ -17,13 +20,13 @@ def main():
         else:
             break
     if choice==1:
-        os.system('cls')
+        system('cls')
         PlayerDetails()
     elif choice==2:
-        os.system('cls')
+        system('cls')
         Claninfo()
     elif choice==3:
-        os.system('cls')
+        system('cls')
         CombatLvlCalc()
     elif choice==4:
         loop=False
@@ -35,7 +38,7 @@ def PlayerDetails():
     
     username = input("Enter a username: ")
     
-    runedata = requests.get("https://apps.runescape.com/runemetrics/profile/profile?user=" + username + "&activities=20")
+    runedata = get("https://apps.runescape.com/runemetrics/profile/profile?user=" + username + "&activities=20")
     print("Name: " + runedata.json()["name"])
     print("Total Skill Level: " + str(runedata.json()["totalskill"]))
     print("Combat Level: " + str(runedata.json()["combatlevel"]))
@@ -51,7 +54,7 @@ def Claninfo():
     clanname = input("Enter clan name: ")
 
     # reading csv file from url  
-    data = pandas.read_csv("http://services.runescape.com/m=clan-hiscores/members_lite.ws?clanName=" + clanname, "utf-8", engine="python") 
+    data = read_csv("http://services.runescape.com/m=clan-hiscores/members_lite.ws?clanName=" + clanname, "utf-8", engine="python") 
   
     # dropping null value columns to avoid errors 
     data.dropna(inplace = True) 
@@ -81,7 +84,7 @@ def CombatLvlCalc():
     print("")
     
     # Get json data and assign skill level to variables
-    runedata = requests.get("https://apps.runescape.com/runemetrics/profile/profile?user=" + username + "&activities=20")
+    runedata = get("https://apps.runescape.com/runemetrics/profile/profile?user=" + username + "&activities=20")
     for skillvalues in runedata.json()["skillvalues"]:
         if skillvalues["id"] == 0:
             attack = skillvalues["level"]
@@ -114,21 +117,21 @@ def CombatLvlCalc():
     # Used formula, with every subcalculation being floored:
     # 1/4 * [1.3 * Max(Att+Str, 2*Mag, 2*Rng) + Def + Hp + Pray/2 + Summ/2]
     # The weight of pray/summ for example is 1/8, that of def/hp is 1/4 etc.
-    combatlvl = ((max(attstr, 2*magic, 2*ranged)) * 13/10 + defence + constitution + math.floor(prayer/2) + math.floor(summoning/2))/4
-    HpDef = math.ceil((1 - (combatlvl % 1)) * 4)
-    PraySumm = math.ceil((1 - (combatlvl % 1)) * 8)
+    combatlvl = ((max(attstr, 2*magic, 2*ranged)) * 13/10 + defence + constitution + floor(prayer/2) + floor(summoning/2))/4
+    HpDef = ceil((1 - (combatlvl % 1)) * 4)
+    PraySumm = ceil((1 - (combatlvl % 1)) * 8)
 
     if attstr >= 2*magic and attstr >= 2*ranged:
-        AttStr = math.ceil((1 - (combatlvl % 1)) / 0.325)
-        Mage = math.ceil((attstr - magic * 2 ) / 2 + (1 - (combatlvl % 1 )) / 0.65)
-        Range = math.ceil((attstr - ranged * 2) / 2 + (1 - (combatlvl % 1)) / 0.65)
+        AttStr = ceil((1 - (combatlvl % 1)) / 0.325)
+        Mage = ceil((attstr - magic * 2 ) / 2 + (1 - (combatlvl % 1 )) / 0.65)
+        Range = ceil((attstr - ranged * 2) / 2 + (1 - (combatlvl % 1)) / 0.65)
     else:
         # calculate att/str levels needed for combat level up:
         # first calculate how many levels to get to make your combat melee-based, then add the amount of levels needed from there.
-        AttStr = max(ranged, magic) * 2 - attstr + math.ceil((1 - (combatlvl % 1)) / 0.325)
+        AttStr = max(ranged, magic) * 2 - attstr + ceil((1 - (combatlvl % 1)) / 0.325)
 
         # store this value in variable Mage first: assume mage-based combat first
-        Mage = math.ceil(( 1 - (combatlvl % 1)) / 0.65 )
+        Mage = ceil(( 1 - (combatlvl % 1)) / 0.65 )
         if ranged > magic:
             # move the calculated value to variable Range: the combat is range-based
             Range = Mage
@@ -138,7 +141,7 @@ def CombatLvlCalc():
             # same logic again
             Range = magic - ranged + Mage
 
-    print("\nYour current combat level is " + str(math.floor(combatlvl)) + ".\nFor level " + str(math.floor(combatlvl) + 1) + " you need one of the folowing:\n")
+    print("\nYour current combat level is " + str(floor(combatlvl)) + ".\nFor level " + str(floor(combatlvl) + 1) + " you need one of the folowing:\n")
     print("- " + str(HpDef) + " Constitution or Defence levels")
     print("- " + str((prayer % 2 == 0 and PraySumm+1 or PraySumm)) + " Prayer levels")
     print("- " + str((summoning % 2 == 0 and PraySumm+1 or PraySumm)) + " Summoning levels")
